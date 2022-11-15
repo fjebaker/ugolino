08584a24174b79f91630f821f8c455dccb816323
fjebaker <fergusbkr@gmail.com>
2022-11-15 10:24:33 +0000

    added rss digest

---
+++ ugolino/digests/rss.py
@@ -0,0 +1,25 @@
+from typing import List
+from ugolino.digests import Digest
+from ugolino import Conference
+
+from feedgen.feed import FeedGenerator
+
+class RSSDigest(Digest):
+    def setup(self):
+        self.generator = FeedGenerator()
+        self.generator.language("en")
+        self.generator.title("astro-feed")
+        self.generator.link(href="todo", rel="self")
+        self.generator.description("todo")
+
+    def drain(self) -> str:
+        rssfeed  = self.generator.rss_str(pretty=True)
+        return rssfeed.decode()
+
+    def conference(self, conf: Conference):
+        entry = self.generator.add_entry()
+        entry.title(f"{conf.date}: {conf.name}")
+        entry.link(href=conf.link, rel="alternate")
+        entry.link(href=conf.source, rel="related")
+        entry.description(f"{conf.location}")
+        entry.content(conf.description)
