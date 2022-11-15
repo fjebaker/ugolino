6d8caed5039d9c0f4726dd876b8ea314ac449298
fjebaker <fergusbkr@gmail.com>
2022-11-15 13:59:30 +0000

    cleanup

--- ugolino/digests/rss.py
+++ ugolino/digests/rss.py
@@ -10,8 +10,8 @@ class RSSDigest(Digest):
         self.generator = FeedGenerator()
         self.generator.language("en")
         self.generator.title("astro-feed")
-        self.generator.link(href="todo", rel="self")
-        self.generator.description("todo")
+        self.generator.link(href="https://github.com/astro-grou-bristol/astro-feeds", rel="self")
+        self.generator.description("Astrophysics and astronomy conferences, aggregated from different sources.")
 
     def drain(self) -> str:
         rssfeed = self.generator.rss_str(pretty=True)
@@ -21,7 +21,7 @@ class RSSDigest(Digest):
         entry = self.generator.add_entry()
         entry.title(f"{conf.format_date()}: {conf.name}")
         if conf.link:
-            entry.link(href=conf.link.text, rel="alternate")
-        entry.link(href=conf.source, rel="related")
+            entry.link(href=conf.link.text, rel="related")
+        entry.link(href=conf.source, rel="via")
         entry.description(f"{conf.location}")
         entry.content(conf.description)
